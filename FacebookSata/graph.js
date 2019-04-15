c3f761fae70b420a6caea6d57119931beac98917
Celine KIm <celinekim3146@gmail.com>
2019-04-15 08:30:00 -0500

    Create graph.js

---
+++ FacebookSata/graph.js
@@ -0,0 +1,10 @@
+zingchart.render({
+    id:"myChart",
+    output:"svg",
+    cache:{
+        data:false
+    },
+    height : "100%",
+    width:"100%",
+    dataurl:"//demos.zingchart.com/view/ICA2JL8W/noncachedchart.json"
+});
